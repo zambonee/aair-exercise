5c416a6149590411354302d381be0f868a350946
Eliza Huang <eliza.huang@alaskaair.com>
2021-06-16 17:57:25 -0700

    update: new currency converter image and fix unit tests

--- server/app.test.js
+++ server/app.test.js
@@ -5,19 +5,19 @@ describe('Test /api/currencyConverter', () => {
   test('Returns correct conversion for USD to USD', async () => {
     const response = await request(app).get('/api/currencyConverter?from=USD&to=USD&amount=10');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toBe('10');
+    expect(response.body.amount).toBe(10);
   });
 
   test('Returns correct conversion for USD to CAD', async () => {
     const response = await request(app).get('/api/currencyConverter?from=USD&to=CAD&amount=10');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toBe('12.07');
+    expect(response.body.amount).toBe(12.07);
   });
 
   test('Returns correct conversion for USD to DZD', async () => {
     const response = await request(app).get('/api/currencyConverter?from=USD&to=DZD&amount=10');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toBe('975.6');
+    expect(response.body.amount).toBe(975.6);
   });
 });
 
