686bf8a225834e05d11f1fd72dab4894812dc0a7
JoshCutler <Josh.Cutler@wizards.com>
2021-06-19 12:48:48 -0700

    locationtocurrency endpoint

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const ratesByCountryName = require('./utils/conversionRates');
 
+const axios = require('axios')
+
 app.get('/api', (req, res) => {
   res.status(200).json({ message: 'Hello from api!' });
 });
@@ -22,18 +24,18 @@ app.get('/api/currencyConverter', (req, res) => {
   const amountCurrency = req.query.amount;
 
   if (!Number(amountCurrency)) {
-    return res.status(400).send({ message: 'invalid "amount" param' });
+    return res.status(400).send('invalid "amount" param');
   }
   const rates = Object.values(ratesByCountryName);
 
   const fromConversionRate = rates.find(rate => rate.currencyCode === fromCurrency);
   if (!fromConversionRate) {
-    return res.status(400).send({ message: 'invalid "from" param' });
+    return res.status(400).send('invalid "from" param');
   }
 
   const toConversionRate = rates.find(rate => rate.currencyCode === toCurrency);
   if (!toConversionRate) {
-    return res.status(400).send({ message: 'invalid "to" param' });
+    return res.status(400).send('invalid "to" param');
   }
 
   const newConversionRate = 1.0 / fromConversionRate['rateFromUSDToCurrency'] * toConversionRate['rateFromUSDToCurrency'];
@@ -52,8 +54,26 @@ app.get('/api/currencyConverter', (req, res) => {
  * Returns the user's local currency type based on the user geolocation.
 */
 app.get('/api/locationToCurrency', (req, res) => {
+  const ipAddress = req.ip;
+  if (!ipAddress) {
+    return res.status(400).send('could not get the request IP address');
+  }
 
-  res.status(503).send('Not Implemented');
+  const uri = `https://ipapi.co/${ipAddress}/currency`;
+  axios.get(uri)
+  .then((response) => {
+    const responseObject = {
+      currency: response.data,
+    };
+    return res.status(200).send(responseObject);
+  })
+  .catch((error) => {
+    console.log(error);
+    if (error.status == 404) {
+      return res.status(404).send('could not get currency for region');
+    }
+    return res.status(500).send('unexpected error');
+  });
 });
 
 module.exports = app;
