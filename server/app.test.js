9aefc197bba9fb49d1c563d5f297285c6c330d29
JoshCutler <Josh.Cutler@wizards.com>
2021-06-19 10:35:50 -0700

    add error handling tests to server

--- server/app.test.js
+++ server/app.test.js
@@ -19,6 +19,36 @@ describe('Test /api/currencyConverter', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body.amount).toBe(975.6);
   });
+
+  test('Returns error when from currency is invalid', async() => {
+    const response = await request(app).get('/api/currencyConverter?from=FOO&to=USD&amount=10');
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('Returns error when from currency is null', async() => {
+    const response = await request(app).get('/api/currencyConverter?from=&to=USD&amount=10');
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('Returns error when to currency is invalid', async() => {
+    const response = await request(app).get('/api/currencyConverter?from=USD&to=FOO&amount=10');
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('Returns error when to currency is null', async() => {
+    const response = await request(app).get('/api/currencyConverter?from=USD&to=&amount=10');
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('Returns error when amount is invalid', async() => {
+    const response = await request(app).get('/api/currencyConverter?from=USD&to=USD&amount=FOO');
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('Returns error when amount is null', async() => {
+    const response = await request(app).get('/api/currencyConverter?from=USD&to=USD&amount=');
+    expect(response.statusCode).toBe(400);
+  });
 });
 
 describe('Test /api/locationToCurrency', () => {
